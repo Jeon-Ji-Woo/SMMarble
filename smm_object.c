2a1fe93bd7dd2b2d2869d9169ebb755a69dd5fd8
Jeon-Ji-Woo <jiwoo3514@sookmyung.ac.kr>
2023-12-21 23:57:12 +0900

    SMMmarble 과제

---
+++ smm_object.c
@@ -0,0 +1,96 @@
+//
+//  smm_node.c
+//  SMMarble
+//
+//  Created by Juyeop Kim on 2023/11/05.
+//
+
+#include "smm_common.h"
+#include "smm_object.h"
+#include <string.h>
+
+#define MAX_NODETYPE    7
+#define MAX_GRADE       9
+#define MAX_NODE        100
+
+
+static char smmNodeName[SMMNODE_TYPE_MAX][MAX_CHARNAME] = {
+       "강의",
+       "식당",
+       "실험실",
+       "집",
+       "실험실로이동",
+       "음식찬스",
+       "축제시간"
+};
+
+char* smmObj_getTypeName(int type)
+{
+      return (char*)smmNodeName[type];
+}
+
+typedef eum smmObjType
+{
+	smmObjType_board = 0;
+	smmObjType_card,
+	smmObjType_grade
+} smmObjType_e;
+
+
+typedef eum smmObjGrade
+{
+	smmObjGrade_Ap = 0;
+	smmObjGrade_A0,
+	smmObjGrade_Am,
+	smmObjGrade_Bp
+	smmObjGrade_B0
+	smmObjGrade_Bm
+} smmObjType_e;
+
+//1. 구조체 형식 정의
+typedef struct smmObject {
+       char name[MAX_CHARNAME];
+       int type;
+       int credit;
+       int energy;
+       smmObjGrade_e grade;
+} smmObject_t;
+
+//2. 구조체 배열 변수 정의 
+//static smmObject_t smm_node[MAX_NODE];
+
+//static int smmObj_noNode = 0;
+
+//3. 관련 함수 변경 
+//object generation
+void smmObj_genNode(char* name, int type, int credit, int energy)
+{    
+    strcpy(smm_node[smmObj_noNode].name, name);
+    smm_node[smmObj_noNode].type = type;
+    smm_node[smmObj_noNode].credit = credit;
+    smm_node[smmObj_noNode].energy = energy;
+    
+    smmObj_noNode++;
+}
+
+//3. 관련 함수 변경 
+char* smmObj_getNodeName(int node_nr)
+{
+    return smm_node[node_nr].name;
+}
+
+//3. 관련 함수 변경 
+int smmObj_getNodeType(int node_nr)
+{
+    return smm_node[node_nr].type;
+}
+
+int smmObj_getNodeCredit(int node_nr)
+{
+    return smm_node[node_nr].credit;
+}
+
+int smmObj_getNodeEnergy(int node_nr)
+{
+    return smm_node[node_nr].energy;
+}
